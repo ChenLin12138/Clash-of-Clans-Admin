57308d9925c6d9336f0597fb889df429333556c7
ChenLin12138 <scorpion_chenlin@163.com>
2019-02-26 21:55:24 +0800

    引入Java8特性

--- src/main/java/com/chenlin/cocadmin/controller/ClanController.java
+++ src/main/java/com/chenlin/cocadmin/controller/ClanController.java
@@ -3,8 +3,6 @@ package com.chenlin.cocadmin.controller;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.chenlin.cocadmin.entities.Clandetails;
 import com.chenlin.cocadmin.entities.Member;
-import com.chenlin.cocadmin.entities.MemberDonationSortWrapper;
 import com.chenlin.cocadmin.entities.Player;
 import com.chenlin.cocadmin.serviceImpl.ClanAPIService;
 import com.chenlin.cocadmin.serviceImpl.PlayerService;
@@ -33,24 +30,25 @@ public class ClanController {
 	public String getMembers(Model model) throws IOException {
 		String clanTag = "92JGQUR";
 		List<Member> members = service.getMembers(clanTag);
-		Set<MemberDonationSortWrapper> set = new TreeSet<MemberDonationSortWrapper>();
-
-		long timeStart = System.currentTimeMillis();
 		Map<String, Player> players = playerservice.getPlayersConcurrent(members);
 
-//		Map<String,Player> players = playerservice.getPlayersSerial(members);
+		members.sort((Member member1, Member member2) -> {
+			int num = member2.getDonations() - member1.getDonations();
+			if (member1.getDonations() < member2.getDonations()) {
+				return 1;
+			} else if (member1.getDonations() > member2.getDonations()) {
+				return -1;
+			}
+			return num == 0 ? member1.getTag().compareTo(member2.getTag()) : num;
+		});
 
-		long timeEnd = System.currentTimeMillis();
-		System.out.println(timeEnd - timeStart);
-//comment for remove player war start		
 		for (Member member : members) {
 			Player player = players.get(member.getTag());
 			member.setWarstar(player.getWarstar());
-			MemberDonationSortWrapper memberDonationSortWrapper = new MemberDonationSortWrapper(member);
-			set.add(memberDonationSortWrapper);
+
 		}
 
-		model.addAttribute("members", set);
+		model.addAttribute("members", members);
 
 		return "members";
 	}
